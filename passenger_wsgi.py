2c0e0fa6568670273c2d02015fe20ddbfbcc1feb
Daniel Davis <davisda4@mail.nlm.nih.gov>
2015-10-07 09:44:59 -0400

    Correct module name

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,5 @@
 import os
 os.environ['MY_STATIC_ROOT'] = '/garethnums/'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'garethnums.settings.prod'
-import garethnames.wsgi
-application = garethnames.wsgi.application
+import garethnums.wsgi
+application = garethnums.wsgi.application
